5d1dfbba845d252744c297ba1db6f8eb9255407e
tnase <alaintona1@gmail.com>
2019-05-27 06:57:04 +0100

    filtre image porno ok
    filtre ok

--- src/app/list-post/list-post.component.ts
+++ src/app/list-post/list-post.component.ts
@@ -18,6 +18,9 @@ export class ListPostComponent implements OnInit {
 
   }
 
+
+
+
   onFieldSelected(files){
      if (files.length === 0)
        return;
@@ -25,14 +28,38 @@ export class ListPostComponent implements OnInit {
     this.imagePath = files;
     reader.readAsDataURL(files[0]);
     reader.onload = (_event) => {
-      this.imgUrl = reader.result;
+    this.imgUrl = reader.result;
     }
       console.log(this.imgUrl);
+
+    this.verifyImage(files[0])
   }
 
   onSubmit(){
 
   this.imgUrl= this.pictureForm.value.picture ;
+
+
+  }
+
+  verifyImage(file){
+    console.log(file)
+            var data = new FormData();
+            data.append('modelId', 'dc2307f6-714c-4ce6-9913-b23e11b398ca');
+            data.append('file', file); // file is a Blob object
+
+            var xhr = new XMLHttpRequest();
+
+            xhr.addEventListener("readystatechange", function () {
+              if (this.readyState === this.DONE) {
+                console.log(this.responseText);
+              }
+            });
+
+            xhr.open("POST", "https://app.nanonets.com/api/v2/ImageCategorization/LabelFile/");
+            xhr.setRequestHeader("authorization", "Basic " + btoa("cNICH5KFzrF6ofTSQCOjGYEsTs-pTAc-:"));
+
+            xhr.send(data);
   }
 
 
